13d16a9d3ae4c6370cc0589cd23e939aab96bf22
codewarrior4 <mayowatope119@gmail.com>
2026-08-20 12:23:32 -0700

    fix: restore stored feature enums

--- app/Http/Controllers/ShowcaseController.php
+++ app/Http/Controllers/ShowcaseController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Enums\DemoAudience;
+use App\Enums\LaunchStage;
+use App\Enums\ThemeVariant;
 use App\Features\EmergencyBrake;
 use App\Features\LaunchMode;
 use App\Features\OperatorConsole;
@@ -22,14 +24,23 @@ class ShowcaseController extends Controller
         $audience = DemoAudience::fromRequest($request->string('audience')->toString());
         $audienceState = Feature::for($audience)->values([PriorityNavigation::class, ShowcaseTheme::class]);
         $globalState = Feature::globally()->values([EmergencyBrake::class, LaunchMode::class, OperatorConsole::class]);
+        $showcaseTheme = $audienceState[ShowcaseTheme::class];
+        $launchStage = $globalState[LaunchMode::class];
+
+        $showcaseTheme = $showcaseTheme instanceof ThemeVariant
+            ? $showcaseTheme
+            : ThemeVariant::from((string) $showcaseTheme);
+        $launchStage = $launchStage instanceof LaunchStage
+            ? $launchStage
+            : LaunchStage::from((string) $launchStage);
 
         return view('showcase', [
             'audience' => $audience,
             'audiences' => DemoAudience::cases(),
             'priorityNavigation' => (bool) $audienceState[PriorityNavigation::class],
-            'showcaseTheme' => $audienceState[ShowcaseTheme::class],
+            'showcaseTheme' => $showcaseTheme,
             'emergencyBrake' => (bool) $globalState[EmergencyBrake::class],
-            'launchStage' => $globalState[LaunchMode::class],
+            'launchStage' => $launchStage,
             'operatorConsole' => (bool) $globalState[OperatorConsole::class],
         ]);
     }
